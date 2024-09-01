7bf8e42cdbeab4c04d6a69a3904cd39ef0cdf966
cykim <cykim8811@snu.ac.kr>
2024-09-01 12:04:08 +0000

    chore: Update long_description_content_type in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.0.2',
     description='High-Level Quantum Computing Simulation in Python',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='cykim8811',
     author_email='cykim8811@snu.ac.kr',
     url='https://github.com/cykim8811/pyqsim',
