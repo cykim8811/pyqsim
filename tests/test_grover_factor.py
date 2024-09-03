4f45446320692b6e348c0838a6896bbf0a5c2bc4
cykim <cykim8811@snu.ac.kr>
2024-09-03 11:25:51 +0000

    feat: Implement factoring test with grover's algorithm

--- tests/test_grover_factor.py
+++ tests/test_grover_factor.py
@@ -1,21 +1,18 @@
 
 import pyqsim as pq
 import numpy as np
+import math
 
 from pyqsim.types import qint
 from pyqsim.gates import h, z
 
+
 a = h(qint(0, size=4))
 b = h(qint(0, size=4))
 
-# pq.bitgate.entangle(a.transform.reg.qubits + b.transform.reg.qubits)
-
-for _ in range(3):
-    z(a+b==15)
+iter_count = round(math.pi / 4 * math.sqrt(2 ** 8 / 2))
+for _ in range(iter_count):
+    z(a * b == 91)
     z((h(a) == 0) & (h(b) == 0))
-
-
-# for i in range(2 ** 4):
-#     print(f"{i:04b} -> {a.transform.reg.qubits[0].quantum_state.state[i]:.2f}")
 
 print(a, b)
